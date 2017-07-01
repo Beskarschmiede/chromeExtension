ab209db22acb8b576344dcca8a69571f7612ea3e
Daniel Igel <Daniel Igel>
2017-07-01 19:06:10 +0200

    update code

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-var host = "amazon.de/?&tag=beskarschmied-21&camp=4582&creative=670874&linkCode=ur1&adid=03MN9N1SRC0AG8BK6ADV&";
+var host = "&tag=beskarschmied-21&camp=4582&creative=670874&linkCode=ur1&adid=03MN9N1SRC0AG8BK6ADV&";
 
 var link = "amazon.";
 
@@ -9,22 +9,9 @@ chrome.tabs.onUpdated.addListener(
     if(tmp.indexOf(host) == -1) {
       if(tmp.indexOf(link) > -1) {
 
-        if( isMainPage(tmp) ) {
-          tmp = tmp.replace(link, host);
-          chrome.tabs.update(tabId, { url: tmp} );
-        }
+        tmp += host;
+        chrome.tabs.update(tabId, { url: tmp} );
       }
     }
   }
 );
-
-function isMainPage(url) {
-  url = url.substring(url.indexOf(link), url.length);
-  url = url.substring(url.indexOf("/") + 1, url.length);
-
-  if(url.length !== 0) {
-    return false;
-  }
-
-  return true;
-}
